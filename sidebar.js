12b8843136a905c8701a65dea86aa8d8989e6ed6
fx4waldi <fx4waldi@yahoo.com>
2017-08-20 23:40:51 +0200

    Version 1.3.0
    -Added ability to sort and create folders #1
    -Restored context menu to input fields
    -Minor changes and bug fixes

--- sidebar.js
+++ sidebar.js
@@ -13,104 +13,224 @@ var prevContext;
 		}
 	});
 	document.getElementById("showAdd").addEventListener("click",showAdd);
+	document.getElementById("addFolder").addEventListener("click",addFolder);
 	document.getElementById("editCancel").addEventListener("click",hideAll);
 	document.getElementById("addCancel").addEventListener("click",hideAll);
 	document.getElementById("deleteCancel").addEventListener("click",hideAll);
+	document.getElementById("editCancelF").addEventListener("click",hideAll);
+	document.getElementById("deleteCancelF").addEventListener("click",hideAll);
 	document.getElementById("options").addEventListener("click",()=>{browser.runtime.openOptionsPage();});
-	window.addEventListener('contextmenu',disableEvent);
+	window.addEventListener('contextmenu',e=>{if(!(e.target.tagName==="INPUT"&&e.target.type==="text"))e.preventDefault();});
+	document.addEventListener('selectstart',e=>{if(!(e.target.tagName==="INPUT"&&e.target.type==="text"))e.preventDefault();});
 	document.getElementById("lista").addEventListener('contextmenu',context);
-	document.getElementById("lista").addEventListener('selectstart',disableEvent);
 	document.getElementById("fillForm").addEventListener("click",fillForm);
 	document.getElementById("statusbar").addEventListener("mousemove",e=>{e.target.removeAttribute("class");});
 	listSite();
 })();
 
 function context(e){
-	hideAll();
-	const a=e.target.parentElement,
-		  id=parseInt(a.getAttribute("id").substr(4)),
-		  c=document.getElementById(`edititem${id}`);
-	if(!c){
-		removeContext(id);
-		let eInput=document.createElement('input');
-			eInput.className="editSite";
-			eInput.id=`edititem${id}`;
-			eInput.type="image";
-			eInput.src="icons/edit.svg";
-			eInput.title=i18n("edit");
-			eInput.addEventListener('click',()=>{
-				showEdit(id);
-			});
-		let dInput=document.createElement('input');
-			dInput.className="deleteSite";
-			dInput.id=`deleteitem${id}`;
-			dInput.type="image";
-			dInput.src="icons/delete.svg";
-			dInput.title=i18n("delete");
-			dInput.addEventListener('click',()=>{
-				showDelete(id);
-			});
-		let sInput=document.createElement('input');
-			sInput.className="scanPage";
-			sInput.id=`scanitem${id}`;
-			sInput.type="image";
-			sInput.src="icons/scan2.svg";
-			sInput.title=i18n("scanWebpage");
-			browser.storage.local.get('sites').then(result=>{
-				let local=result.sites[id];
-				sInput.addEventListener('click',()=>{
-					scanPage(local,id,false,true);
+	const a=e.target.parentElement;
+	if(a.tagName==="A")return;
+	if(a.classList.contains("folder")){
+		const id=parseInt(a.getAttribute("id").substr(6)),
+			  c=document.getElementById(`editFolder${id}`);
+		if(!c){
+			hideAll();
+			removeContext();
+			let dInput=document.createElement('img');
+				dInput.className="deleteFolder";
+				dInput.id=`deleteFolder${id}`;
+				dInput.src="icons/delete.svg";
+				dInput.title=i18n("delete");
+				dInput.addEventListener('click',()=>{
+					showDeleteFolder(`folder${id}`,e.target.childNodes[1].textContent);
 				});
-			});
-		a.insertBefore(dInput,a.firstChild);
-		a.insertBefore(eInput,dInput);
-		a.insertBefore(sInput,eInput);
-		prevContext=id;
+			let eInput=document.createElement('img');
+				eInput.className="editFolder";
+				eInput.id=`editFolder${id}`;
+				eInput.src="icons/edit.svg";
+				eInput.title=i18n("edit");
+				eInput.addEventListener('click',()=>{
+					showEditFolder(`folder${id}`,e.target.childNodes[1].textContent);
+				});
+			e.target.appendChild(dInput);
+			e.target.appendChild(eInput);
+			prevContext=id;
+		}
+	}else{
+		const id=parseInt(a.getAttribute("id").substr(4)),
+			  c=document.getElementById(`edititem${id}`);
+		if(!c){
+			hideAll();
+			removeContext();
+			let eInput=document.createElement('input');
+				eInput.className="editSite";
+				eInput.id=`edititem${id}`;
+				eInput.type="image";
+				eInput.src="icons/edit.svg";
+				eInput.title=i18n("edit");
+				eInput.addEventListener('click',()=>{
+					showEdit(id);
+				});
+			let dInput=document.createElement('input');
+				dInput.className="deleteSite";
+				dInput.id=`deleteitem${id}`;
+				dInput.type="image";
+				dInput.src="icons/delete.svg";
+				dInput.title=i18n("delete");
+				dInput.addEventListener('click',()=>{
+					showDelete(id);
+				});
+			let sInput=document.createElement('input');
+				sInput.className="scanPage";
+				sInput.id=`scanitem${id}`;
+				sInput.type="image";
+				sInput.src="icons/scan2.svg";
+				sInput.title=i18n("scanWebpage");
+				browser.storage.local.get('sites').then(result=>{
+					let local=result.sites[id];
+					sInput.addEventListener('click',()=>{
+						scanPage(local,id,false,true);
+					});
+				});
+			a.insertBefore(dInput,a.firstChild);
+			a.insertBefore(eInput,dInput);
+			a.insertBefore(sInput,eInput);
+			prevContext=id;
+		}
 	}
 }
 
-function removeContext(id){
+function removeContext(){
 	if(prevContext!=undefined){
-		let aParent=document.getElementById(`item${prevContext}`),
-			e1=document.getElementById(`edititem${prevContext}`),
-			e2=document.getElementById(`deleteitem${prevContext}`),
-			e3=document.getElementById(`scanitem${prevContext}`);
-		aParent.removeChild(e1);
-		aParent.removeChild(e2);
-		aParent.removeChild(e3);
+		if(prevContext<10000){
+			let aParent=document.getElementById(`item${prevContext}`),
+				e1=document.getElementById(`edititem${prevContext}`),
+				e2=document.getElementById(`deleteitem${prevContext}`),
+				e3=document.getElementById(`scanitem${prevContext}`);
+			aParent.removeChild(e1);
+			aParent.removeChild(e2);
+			aParent.removeChild(e3);
+		}else{
+			let aParent=document.getElementById(`folder${prevContext}`).firstElementChild,
+				e1=document.getElementById(`editFolder${prevContext}`),
+				e2=document.getElementById(`deleteFolder${prevContext}`);
+			aParent.removeChild(e1);
+			aParent.removeChild(e2);
+		}
 	}
 }
 
-function listSite(){
-	browser.storage.local.get('sites').then(result=>{
+function listSite(send){
+	if(send)browser.runtime.sendMessage({"listSite":true});
+	browser.storage.local.get(['sites','sort']).then(result=>{
 		prevContext=undefined;
 		document.getElementById("lista").textContent="";
 		const sites=result.sites,
+			  sort=result.sort,
 			  list=document.getElementById("lista");
-		sites.forEach((value,i)=>{
-			let iLi=document.createElement('li');
-				iLi.id=`item${i}`;
-				if(value.changed)iLi.className="changed";
-			let iA=document.createElement('a');
-				iA.textContent=value.title;
+		let lastFolder;
+		sort.forEach((value,i)=>{
+			if(value[2]==="item"){
+				let id=parseInt(value[0].substr(4));
+				let iLi=document.createElement('li');
+					iLi.id=value[0];
+					iLi.dataset.row="a"+i;
+					iLi.dataset.folder=value[1];
+					iLi.draggable=true;
+					if(sites[id].changed)iLi.className="changed";
+					iLi.addEventListener('dragstart',dragStart);
+				let iA=document.createElement('a');
+				iA.textContent=sites[id].title;
 				iA.addEventListener('mouseup',e=>{
 					if(e.button!=2){
 						if(e.target.parentNode.classList.contains("changed"))updateBadge(-1);
 						browser.tabs.create({
-							url:`${extURL}view.html?${i}`,
+							url:`${extURL}view.html?${id}`,
 							active:(e.button===1||(e.button===0&&e.ctrlKey===true))?false:true
 						});
-						unchange([i]);
+						unchange([id]);
 						iLi.classList.remove("changed");
 					}
 				});
-			let iImg=document.createElement('img');
-				iImg.className="favicon";
-				iImg.src=value.favicon;
-			iA.insertBefore(iImg,iA.firstChild);
-			iLi.appendChild(iA);
-			list.appendChild(iLi);
+				let iImg=document.createElement('img');
+					iImg.className="favicon";
+					iImg.src=sites[id].favicon;
+					iImg.draggable=false;
+				iA.insertBefore(iImg,iA.firstChild);
+				iLi.appendChild(iA);
+				if(value[1]==="root"){
+					list.appendChild(iLi);
+				}else
+					lastFolder.appendChild(iLi);
+			}else{
+				let iUl=document.createElement('ul');
+					iUl.id=value[0];
+					iUl.dataset.row="a"+i;
+					iUl.dataset.folder="root";
+					iUl.draggable=true;
+					iUl.addEventListener('dragstart',dragStart);
+					iUl.className=value[4]?"folder collapsed":"folder";
+				let iA=document.createElement('a');
+					iA.textContent=value[3];
+				let iImg=document.createElement('img');
+					iImg.className="folderIcon";
+					iImg.src="icons/blank.svg";
+					iImg.draggable=false;
+				iUl.addEventListener("click",e=>{
+					e.target.parentElement.classList.toggle("collapsed");
+					saveSort();
+				});
+				iImg.addEventListener("click",e=>{
+					e.target.parentElement.parentElement.classList.toggle("collapsed");
+					saveSort();
+				});
+				iA.insertBefore(iImg,iA.firstChild);
+				iUl.appendChild(iA);
+				list.appendChild(iUl);
+				lastFolder=iUl;
+			}
 		});
+	});
+}
+
+function showEditFolder(id,name){
+	hideAll("editFolder");
+	document.getElementById("editSaveF").addEventListener("click",()=>{editFolder(id);});
+	document.getElementById("nameFolder").value=name;
+	document.getElementById("editingFolder").classList.remove("hidden");
+}
+
+function editFolder(id){
+	let newName=document.getElementById("nameFolder").value;
+	document.getElementById("editingFolder").classList.add("hidden");
+	document.getElementById(id).firstElementChild.childNodes[1].textContent=newName;
+	saveSort();
+	statusbar(i18n("savedWebpage",newName));
+}
+
+function showDeleteFolder(id,name){
+	hideAll("deleteFolder");
+	document.getElementById("deleteSaveF").addEventListener("click",()=>{deleteFolder(id,name);});
+	document.getElementById("deleteFolderName").textContent=name;
+	document.getElementById("deletingFolder").classList.remove("hidden");
+}
+
+function deleteFolder(id,name){
+	document.getElementById("deletingFolder").classList.add("hidden");
+	browser.storage.local.get('sort').then(result=>{
+		let sort=result.sort;
+		sort.forEach((value,i)=>{
+			if(value[0]===id)
+				sort.splice(i,1);
+		});	
+		sort.forEach((value,i)=>{
+			if(value[1]===id)
+				sort[i][1]="root";
+		});
+		browser.storage.local.set({sort});
+		statusbar(i18n("deletedWebpage",name));
+		listSite(true);
 	});
 }
 
@@ -143,31 +263,41 @@ function editSite(e){
 		sites[e]=Object.assign(sites[e],obj);
 		browser.storage.local.set({sites});
 		statusbar(i18n("savedWebpage",sites[e].title));
-		listSite();
+		listSite(true);
 	});
 }
 
 function showDelete(e){
 	hideAll("delete");
 	browser.storage.local.get('sites').then(result=>{
-		const table=result.sites;
+		const sites=result.sites;
 		document.getElementById("deleteSite").addEventListener("click",()=>{deleteSite(e);});
+		document.getElementById("deleteTitle").textContent=sites[e].title;
+		document.getElementById("deleteUrl").textContent=sites[e].url;
 		document.getElementById("deletingSite").classList.remove("hidden");
-		document.getElementById("deleteTitle").textContent=table[e].title;
-		document.getElementById("deleteUrl").textContent=table[e].url;
 	});
 }
 
 function deleteSite(e){
 	document.getElementById("deletingSite").classList.add("hidden");
-	browser.storage.local.get(['sites','changes']).then(result=>{
+	browser.storage.local.get(['sites','changes','sort']).then(result=>{
 		let sites=result.sites,
-			changes=result.changes;
+			changes=result.changes,
+			sort=result.sort,
+			sSort;
 		statusbar(i18n("deletedWebpage",sites[e].title));
 		sites.splice(e,1);
 		changes.splice(e,1);
-		browser.storage.local.set({sites:sites,changes:changes});
-		listSite();
+		if(sort){
+			sort.forEach((value,i)=>{
+				id=parseInt(value[0].substr(4));
+				if(id===e)sSort=i;
+				else if(id>e)sort[i][0]=`item${id-1}`;
+			});
+			sort.splice(sSort,1);
+		}
+		browser.storage.local.set({sites:sites,changes:changes,sort:sort});
+		listSite(true);
 	}).then(()=>{
 		browser.runtime.sendMessage({"deletedSite":true,"id":e});
 	});
@@ -223,6 +353,14 @@ function hideAll(e){
 		document.getElementById("deleteTitle").textContent="";
 		document.getElementById("deleteUrl").textContent="";
 	}
+	if(e!="editFolder"){
+		document.getElementById("editingFolder").classList.add("hidden");
+		document.getElementById("nameFolder").value="";
+	}
+	if(e!="deleteFolder"){
+		document.getElementById("deletingFolder").classList.add("hidden");
+		document.getElementById("deleteFolderName").textContent="";
+	}
 }
 
 function statusbar(e){
@@ -230,10 +368,6 @@ function statusbar(e){
 	statusbar.textContent=e;
 	statusbar.className="visible"
 	setTimeout(()=>{if(statusbar.textContent===e)statusbar.removeAttribute("class");},5000);
-}
-
-function disableEvent(e){
-	e.preventDefault();
 }
 
 browser.runtime.onMessage.addListener(run);
@@ -253,6 +387,7 @@ function translate(){
 	document.getElementById("scanSites").title=i18n("scanWebpage");
 	document.getElementById("openSite").title=i18n("openWebpage");
 	document.getElementById("showAdd").title=i18n("addWebpage");
+	document.getElementById("addFolder").title=i18n("addFolder");
 	document.getElementById("options").title=i18n("options");
 	document.getElementById("addWebpage").textContent=i18n("addWebpage");
 	document.getElementById("addressA").textContent=i18n("address");
@@ -266,6 +401,14 @@ function translate(){
 	document.getElementById("modeTitleA").textContent=i18n("modeTitle");
 	document.getElementById("modeTitleE").textContent=i18n("modeTitle");
 	document.getElementById("deleteWebpage").textContent=i18n("deleteWebpage");
+	document.getElementById("editFolder").textContent=i18n("editFolder");
+	document.getElementById("nFolder").textContent=i18n("name");
+	document.getElementById("editCancelF").textContent=i18n("cancel");
+	document.getElementById("editSaveF").textContent=i18n("save");
+	document.getElementById("deleteFolder").textContent=i18n("deleteFolder");
+	document.getElementById("deleteFolderDesc").textContent=i18n("deleteFolderDesc");
+	document.getElementById("deleteCancelF").textContent=i18n("cancel");
+	document.getElementById("deleteSaveF").textContent=i18n("delete");
 	let selectFreqA=document.getElementById("aFreq").options;
 		selectFreqA[0].text=i18n("1Hour");
 		selectFreqA[1].text=i18n("4Hours");
